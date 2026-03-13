b1d5e516801fbe1b9b8f7451b6309805a7a0fdc7
Eyal Amir <eyalamirmusic@gmail.com>
2026-03-12 17:38:14 -0700

    Added higher level functions to register tests

--- Lib/NanoTestDiscoverTests.cmake
+++ Lib/NanoTestDiscoverTests.cmake
@@ -1,12 +1,20 @@
+# nano_add_executable(TARGET source1 [source2 ...])
+#
+# Creates a test executable, links NanoTest, and discovers individual tests.
+#
+# Usage:
+#   nano_add_executable(MyTests tests.cpp)
+
+function(nano_add_executable TARGET)
+    add_executable(${TARGET} ${ARGN})
+    target_link_libraries(${TARGET} PRIVATE NanoTest)
+    nano_discover_tests(${TARGET})
+endfunction()
+
 # nano_discover_tests(TARGET)
 #
 # Discovers individual test cases registered in a NanoTest executable
 # and adds each one as a separate CTest test.
-#
-# Usage:
-#   add_executable(MyTests tests.cpp)
-#   target_link_libraries(MyTests PRIVATE NanoTest)
-#   nano_discover_tests(MyTests)
 
 function(nano_discover_tests TARGET)
     add_custom_command(
